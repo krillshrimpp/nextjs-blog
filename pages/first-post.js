883dd58a97c9092635c5a0ac8afbfdf883df6953
roqkf.lhs@gmail.com <roqkf.lhs@gmail.com>
2023-04-06 15:58:12 +0900

    2

---
+++ pages/first-post.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <p1>Hi, my name is Lee Hoseop. </p1>
+    </>
+  );
+} 
